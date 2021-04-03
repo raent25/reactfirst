079a721c2c665a214e86a1530f15baf8791617a9
raent <raent25@gmail.com>
2021-04-03 10:30:45 +0300

    practical task#1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 "use strict";
+let numberOfFirst = +prompt("How many movies have you seen?", '')
+let personalMovieBD = {
+    count: numberOfFirst,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
+let LastMovie = prompt("Last watched movie?", '')
+let Rating = prompt("Rating?", '')
+personalMovieBD.movies[LastMovie] = Rating
+console.log(personalMovieBD)
