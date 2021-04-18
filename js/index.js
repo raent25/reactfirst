4da6a9f70eadeab94087bb4807f48db6554ef5ad
raent <raent25@gmail.com>
2021-04-18 16:05:41 +0300

    test

--- js/index.js
+++ js/index.js
@@ -7,7 +7,20 @@ let personalMovieBD = {
     genres: [],
     privat: false
 }
-let LastMovie = prompt("Last watched movie?", '')
-let Rating = prompt("Rating?", '')
-personalMovieBD.movies[LastMovie] = Rating
+
+for (let i = 0; i < 2; i++) {
+    let LastMovie = prompt("Last watched movie?", '')
+    if (LastMovie == '' || LastMovie.length > 50) {
+        do {
+            LastMovie = prompt("You didn`t ansver! Or line length exceeded! Last watched movie?", '')
+        } while (LastMovie == '' || LastMovie.length > 50)
+    }
+    let Rating = prompt("Rating?", '')
+    if (Rating == '') {
+        do {
+            Rating = prompt("You didn`t ansver! Rating?", '')
+        } while (Rating == '')
+    }
+    personalMovieBD.movies[LastMovie] = Rating
+}
 console.log(personalMovieBD)
