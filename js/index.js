53cd7a9caf49dc07bb532bc5c7294bf9d84e9b98
raent <raent25@gmail.com>
2021-04-22 22:38:35 +0300

    practical task#2 part1

--- js/index.js
+++ js/index.js
@@ -7,20 +7,29 @@ let personalMovieBD = {
     genres: [],
     privat: false
 }
-
 for (let i = 0; i < 2; i++) {
     let LastMovie = prompt("Last watched movie?", '')
     if (LastMovie == '' || LastMovie.length > 50) {
         do {
             LastMovie = prompt("You didn`t ansver! Or line length exceeded! Last watched movie?", '')
-        } while (LastMovie == '' || LastMovie.length > 50)
+        } while (LastMovie == '' || LastMovie == null || LastMovie.length > 50)
     }
     let Rating = prompt("Rating?", '')
     if (Rating == '') {
         do {
             Rating = prompt("You didn`t ansver! Rating?", '')
-        } while (Rating == '')
+        } while (Rating == '' || Rating == null)
     }
     personalMovieBD.movies[LastMovie] = Rating
+}
+
+if (personalMovieBD.count > 0 && personalMovieBD.count < 11) {
+    alert("ви дивились мало фільмів")
+} else if (personalMovieBD.count > 10 && personalMovieBD.count < 31) {
+    alert("ви середньостатичстичний глядач")
+} else if (personalMovieBD.count > 30) {
+    alert("ви кіноман")
+} else {
+    alert("вийшла помилка")
 }
 console.log(personalMovieBD)
